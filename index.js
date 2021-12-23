1b3306abb72441412d7ca8c784da2784f393d541
Enis K <aneeshkarki292@gmail.com>
2021-12-23 20:37:27 +1100

    changes

---
+++ index.js
@@ -0,0 +1,17 @@
+const quote = document.querySelector(".quote");
+const authorBy = document.querySelector(".author");
+const tag = document.querySelector(".tag");
+quoteBtn = document.querySelector("button");
+
+//random quote function
+function newQuote(){
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+        console.log(result);
+        quote.innerText = result.content;
+        authorBy.innerText = result.author;
+        tag.innerText = result.tags;
+    })
+}
+
+quoteBtn.addEventListener("click", newQuote);
+
